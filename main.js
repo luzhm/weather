bac4d91241741bc8eb27f71be9ac576a74dbbdf5
Мария Лужецкая <maryam.lug@mail.ru>
2025-12-28 01:17:51 +0300

    обработчик ошибок

--- main.js
+++ main.js
@@ -1,29 +1,22 @@
 console.log('Weather app started');
+
 window.addEventListener('load', () => {
+  const currentLocationSection = document.querySelector('.current-location');
+  const addCitySection = document.querySelector('.add-city');
+
   if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        console.log('Координаты:', position.coords.latitude, position.coords.longitude);
-          const currentLocationSection = document.querySelector('.current-location');
-
-navigator.geolocation.getCurrentPosition(
-  (position) => {
-    const { latitude, longitude } = position.coords;
-    console.log('Координаты:', latitude, longitude);
-
-
-    currentLocationSection.querySelector('.location-title').textContent = 
-      `Текущее местоположение: ${latitude.toFixed(2)}, ${longitude.toFixed(2)}`;
-  },
-
-);
-
+        const { latitude, longitude } = position.coords;
+        currentLocationSection.querySelector('.location-title').textContent = 
+          `Текущее местоположение: ${latitude.toFixed(2)}, ${longitude.toFixed(2)}`;
+        addCitySection.classList.add('hidden');
       },
       (error) => {
-        console.log('Доступ к геолокации отклонён или произошла ошибка:', error.message);
+        addCitySection.classList.remove('hidden');
       }
     );
   } else {
-    console.log('Геолокация не поддерживается браузером');
+    addCitySection.classList.remove('hidden');
   }
 });
