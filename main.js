caf87e7e21d9fdb07b08528a7e78aee29d5f8160
Мария Лужецкая <maryam.lug@mail.ru>
2025-12-28 01:07:01 +0300

    запрос геолокации

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 console.log('Weather app started');
+window.addEventListener('load', () => {
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log('Координаты:', position.coords.latitude, position.coords.longitude);
+          
+      },
+      (error) => {
+        console.log('Доступ к геолокации отклонён или произошла ошибка:', error.message);
+      }
+    );
+  } else {
+    console.log('Геолокация не поддерживается браузером');
+  }
+});
