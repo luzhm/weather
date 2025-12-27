f4c1b9590ea58f2fe6794a52f185eade6d79072f
Мария Лужецкая <maryam.lug@mail.ru>
2025-12-28 01:08:54 +0300

    получение геолокации

--- main.js
+++ main.js
@@ -4,7 +4,20 @@ window.addEventListener('load', () => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         console.log('Координаты:', position.coords.latitude, position.coords.longitude);
-          
+          const currentLocationSection = document.querySelector('.current-location');
+
+navigator.geolocation.getCurrentPosition(
+  (position) => {
+    const { latitude, longitude } = position.coords;
+    console.log('Координаты:', latitude, longitude);
+
+
+    currentLocationSection.querySelector('.location-title').textContent = 
+      `Текущее местоположение: ${latitude.toFixed(2)}, ${longitude.toFixed(2)}`;
+  },
+
+);
+
       },
       (error) => {
         console.log('Доступ к геолокации отклонён или произошла ошибка:', error.message);
